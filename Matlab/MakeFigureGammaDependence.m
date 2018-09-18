9f24d7a6cbdd24df4e610eb55d950d12a350e66d
JeffreyEarly <jeffrey@jeffreyearly.com>
2018-09-17 20:54:19 -0700

    better fitting to find gamma scaling

--- Matlab/MakeFigureGammaDependence.m
+++ Matlab/MakeFigureGammaDependence.m
@@ -15,13 +15,15 @@ elseif slope == -4
 end
 
 result_stride = [4;8;16;32;64;128];
-ensembles = 15*ones(size(result_stride));;
+ensembles = 5*ones(size(result_stride));
 totalEnsembles = sum(ensembles);
 
-measured_dof = zeros(length(result_stride)*totalEnsembles,1);
-d_gamma = zeros(length(result_stride)*totalEnsembles,1);
+measured_dof = zeros(totalEnsembles,1);
+d_gamma = zeros(totalEnsembles,1);
 
 measured_dof_mean = zeros(length(result_stride),1);
+measured_dof_median = zeros(length(result_stride),1);
+measured_dof_std = zeros(length(result_stride),1);
 d_gamma_mean = zeros(length(result_stride),1);
 
 iteration = 0;
@@ -40,11 +42,7 @@ for i=1:length(result_stride)
         
         indices = 1:stride:floor(shortenFactor*length(data.t));
         fprintf('Using %d points with stride %d\n', length(indices), stride);
-        if (shouldUseObservedSignalOnly == 1)
-            indicesAll = indices;
-        else
-            indicesAll = 1:max(indices);
-        end
+        indicesAll = indices;
         
         
         epsilon_x = data.position_error*randn(length(indices),1);
@@ -73,21 +71,45 @@ for i=1:length(result_stride)
     startIndex = cum_ensembles(i) - ensembles(i) + 1;
     endIndex = cum_ensembles(i);
     measured_dof_mean(i) = mean( measured_dof(startIndex:endIndex) );
+    measured_dof_median(i) = median( measured_dof(startIndex:endIndex) );
+    measured_dof_std(i) = std( measured_dof(startIndex:endIndex) );
     d_gamma_mean(i) = mean( d_gamma(startIndex:endIndex) );
 end
 
-D = 1;
-[p,~,mu]=polyfit(d_gamma,measured_dof,D);
-slope = factorial(D)*p(1)/mu(2)^D
 
-[p,~,mu]=polyfit(d_gamma_mean(2:6),measured_dof_mean(2:6),D);
-slope = factorial(D)*p(1)/mu(2)^D
-intercept = p(2)-p(1)*mu(1)/mu(2)
+% Best fit to a straight line, using the estimate of the standard error for
+% sigma. From Press et al page 662
+sigma2 = (measured_dof_std.*measured_dof_std)./ensembles;
+x = d_gamma_mean;
+y = measured_dof_mean;
+
+S = sum(1./sigma2);
+Sx = sum(x./sigma2);
+Sy = sum(y./sigma2);
+Sxx = sum((x.*x)./sigma2);
+Sxy = sum((x.*y)./sigma2);
+Delta = S*Sxx - Sx*Sx;
+a = (Sxx*Sy - Sx*Sxy)/Delta;
+b = (S*Sxy - Sx*Sy)/Delta;
 
 figure
 scatter(d_gamma,measured_dof), hold on
-plot(d_gamma_mean, slope*d_gamma_mean + intercept)
+plot(d_gamma_mean, b*d_gamma_mean + a)
+xlabel('dof from \Gamma')
+ylabel('measured dof')
+title(sprintf('best fit slope: %f',b))
 
-figure
-scatter(d_gamma_mean,measured_dof_mean), hold on
-plot(d_gamma_mean, slope*d_gamma_mean + intercept)
+
+% D = 1;
+% [p,~,mu]=polyfit(d_gamma,measured_dof,D);
+% slope = factorial(D)*p(1)/mu(2)^D;
+% 
+% [p,~,mu]=polyfit(d_gamma_mean,measured_dof_median,D);
+% slope = factorial(D)*p(1)/mu(2)^D
+% intercept = p(2)-p(1)*mu(1)/mu(2)
+% 
+% figure
+% scatter(d_gamma_mean,measured_dof_median), hold on
+% plot(d_gamma_mean, slope*d_gamma_mean + intercept)
+% plot(d_gamma_mean, b*d_gamma_mean + a)
+
