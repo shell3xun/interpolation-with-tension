c89834884d8a187115bf31405e41f6c316836466
JeffreyEarly <jeffrey@jeffreyearly.com>
2018-10-17 14:25:15 -0700

    Adding a few new optimal parameter tables and figure

---
+++ Matlab/MakeFigureSignalAndNoiseSpectraWithSmootherOfSplineDegrees.m
@@ -0,0 +1,182 @@
+% Setting the order of the spline sets the 'interpolation fall off', by
+% which I mean that it sets the slope that the recovered signal falls off
+% at, once it becomes garbage.
+%
+% I think this falloff is with a slope *at least* as steep as the actual
+% signal slope for the lower frequency garbage.
+%
+% The -2 slope, needs S=1
+% The -4 slope, needs S=2
+% Anything else is either too steep or too shallow in the spectrum
+% The -3 slope is what you'd expect: neither works well
+%
+% Coherence would test signal to recovered signal
+
+addpath('./support');
+% AMS figure widths, given in picas, converted to points (1 pica=12 points)
+scaleFactor = 1;
+LoadFigureDefaults
+
+slope = -2;
+numDerivs = 1;
+S = 3;
+T = 3;
+K = S+1;
+
+if slope == -2
+    data = load('sample_data/SyntheticTrajectories.mat');
+elseif slope == -3
+    data = load('sample_data/SyntheticTrajectoriesSlope3.mat');
+elseif slope == -4
+    data = load('sample_data/SyntheticTrajectoriesSlope4.mat');
+end
+
+t = data.t;
+x = data.x;
+y = data.y;
+epsilon_x = data.epsilon_x;
+epsilon_y = data.epsilon_y;
+position_error = data.position_error;
+
+timescale = 60;
+indices = find( t/timescale >= 0 & t/timescale <= 42);
+
+markersize  = 2*scaleFactor;
+stride = 10; % we start by decimating the signal
+range = indices(1:stride:end);
+
+shouldUseObservedSignalOnly = 0;
+result_stride = [1; 10; 100];
+% result_stride = 100;
+dof = zeros(length(result_stride),1);
+
+FigureSize = [50 50 figure_width_large+7 300*scaleFactor];
+
+fig1 = figure('Units', 'points', 'Position', FigureSize);
+set(gcf,'PaperPositionMode','auto')
+set(gcf, 'Color', 'w');
+fig1.PaperUnits = 'points';
+fig1.PaperPosition = FigureSize;
+fig1.PaperSize = [FigureSize(3) FigureSize(4)];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% plot the signal and noise separately
+
+D = TensionSpline.FiniteDifferenceMatrixNoBoundary(numDerivs,t,1);
+
+dt = t(2)-t(1);
+cv = D*(x + sqrt(-1)*y);
+cepsilon = D*(epsilon_x + sqrt(-1)*epsilon_y);
+[psi,lambda]=sleptap(size(cv,1));
+[f,spp,snn,spn]=mspec(dt,cv,psi, 'cyclic');
+[f,spp_e,snn_e,spn_e]=mspec(dt,cepsilon,psi,'cyclic');
+
+ylimit = [1e-4 4e2];
+
+sp1 = subplot(2,1,1);
+sp2 = subplot(2,1,2);
+
+set(fig1, 'currentaxes', sp1)
+plot(f*timescale,vmean([snn, spp],2), 'k', 'LineWidth', 2);
+hold on
+plot(f*timescale,vmean([snn_e, spp_e],2), 'r', 'LineWidth', 2);
+xlog, ylog
+xlim([min(f*timescale) max(f*timescale)])
+ylim(ylimit)
+
+subplot(sp2)
+plotHandles(1) = plot(1, nan, 'k', 'LineWidth', 2); hold on;
+plotHandles(2) = plot(1, nan, 'r', 'LineWidth', 2);
+
+for i=1:3
+    stride = 10;
+    % Reduce the total length in some cases
+    if (stride < 10)
+        shortenFactor = stride/10;
+        shortenFactor = 1;
+    else
+        shortenFactor = 1;
+    end
+    
+    indices = 1:stride:floor(shortenFactor*length(data.t));
+    fprintf('dT = %d --- Using %d points with stride %d\n', stride*dt, length(indices), stride);
+    if (shouldUseObservedSignalOnly == 1)
+        indicesAll = indices;
+    else
+        indicesAll = 1:max(indices);
+    end
+    epsilon_x = data.epsilon_x(indices);
+    epsilon_y = data.epsilon_y(indices);
+    x_obs = data.x(indices) + epsilon_x;
+    y_obs = data.y(indices) + epsilon_y;
+    t_obs = data.t(indices);
+    sigma = data.position_error;
+        
+    spline_x = TensionSpline(t_obs,x_obs,sigma,'S', i, 'T', i, 'knot_dof', 'auto');
+    spline_y = TensionSpline(t_obs,y_obs,sigma,'S', i, 'T', i, 'knot_dof', 'auto');
+%     TensionSpline.MinimizeExpectedMeanSquareError(spline_x);
+%     TensionSpline.MinimizeExpectedMeanSquareError(spline_y);
+    
+    TensionSpline.MinimizeMeanSquareError(spline_x,data.t(indicesAll),data.x(indicesAll));
+    TensionSpline.MinimizeMeanSquareError(spline_y,data.t(indicesAll),data.y(indicesAll));
+    
+    
+    x_smooth = spline_x(t(indicesAll));
+    y_smooth = spline_y(t(indicesAll));
+    
+    rms_error_x(i) =  sqrt(mean(mean(  (data.x(indicesAll) - x_smooth).^2,2 ),1));
+    rms_error_y(i) =  sqrt(mean(mean(  (data.y(indicesAll) - y_smooth).^2,2 ),1));
+    fprintf('rms error (x,y)=(%f,%f)\n',rms_error_x, rms_error_y);
+    
+    D = TensionSpline.FiniteDifferenceMatrixNoBoundary(numDerivs,t(indicesAll),1);
+    u_smooth = D*x_smooth;
+    v_smooth = D*y_smooth;
+    
+    dof(i) = (spline_x.DOFFromVarianceOfTheMean + spline_y.DOFFromVarianceOfTheMean)/2;
+    f_limit = 1/(2*dof(i)*(t_obs(2)-t_obs(1)));
+    
+    
+    subplot(sp1)
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % plot the spline fit
+    cv = u_smooth+sqrt(-1)*v_smooth;
+    [psi,lambda]=sleptap(size(cv,1));
+    [f,spp,snn,spn]=mspec(dt,cv,psi,'cyclic');
+    ax = gca;
+    CO = ax.ColorOrderIndex;
+    plot(f*timescale,vmean([snn, spp],2), 'LineWidth', 0.5*scaleFactor);
+    ylimits = ylim;
+%     plot(timescale*f_limit*[1 1],ylimits,'k--')
+    
+    
+    subplot(sp2)
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % plot the coherence of the spline fit with the true signal
+    [f,sxx,syy,sxy]=mspec(dt,u_smooth,D*data.x(indicesAll),psi,'cyclic');
+    gamma=frac(abs(sxy).^2,sxx.*syy);
+    
+    plotHandles(2+i) = plot(f*timescale,RunningAverage(gamma,20)); hold on
+    xlog
+    xlim([min(f*timescale) max(f*timescale)])
+    ylim([0 1])
+    
+    ylimits = ylim;
+%     plot(timescale*f_limit*[1 1],ylimits,'k--')
+end
+
+subplot(sp1)
+ylabel('power (m^2/s)', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+
+subplot(sp2)
+ylabel('coherence', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+xlabel('cycles per minute', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+mylegend = legend(plotHandles,'signal', 'noise', sprintf('S=1 (%.2f m rmse)',rms_error_x(1)), sprintf('S=2 (%.2f m rmse)',rms_error_x(2)), sprintf('S=3 (%.2f m rmse)',rms_error_x(3)),'Location','southwest');
+xlabel('cycles per minute', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+
+packfig(2,1)
+
+print('-depsc2', sprintf('../figures/synthetic_process_and_spectrum_slope%ddegreeVaried.eps',abs(slope)))
+return
+
---
+++ Matlab/MakeTableOptimalTensionOrder.m
@@ -0,0 +1,165 @@
+% The purpose here is to establish if there is any merit in applying
+% tension at something other than T=S. We let T=1..S 
+shouldLoadExistingTable = 1;
+
+filename = 'OptimalTensionOrderTable.mat';
+
+if shouldLoadExistingTable == 1
+    load(filename);
+else
+    slopes = [-2; -3; -4];
+    S_range = 1:5;
+    result_stride = 2.^(2:7)';
+    totalEnsembles = 5;
+    
+    totalSlopes = length(slopes);
+
+    lambda_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    rms_error_obs_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    rms_error_all_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    dof_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    Q_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    
+    lambda_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    rms_error_obs_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    rms_error_all_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    dof_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    Q_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+        
+    for iSlope = 1:length(slopes)
+        
+        slope = slopes(iSlope);
+        fprintf('slope %d, ',slope);
+        
+        if slope == -2
+            data = load('sample_data/SyntheticTrajectories.mat');
+            outputFile = 'OptimalParameters.mat';
+        elseif slope == -3
+            data = load('sample_data/SyntheticTrajectoriesSlope3.mat');
+            outputFile = 'OptimalParametersSlope3.mat';
+        elseif slope == -4
+            data = load('sample_data/SyntheticTrajectoriesSlope4.mat');
+            outputFile = 'OptimalParametersSlope4.mat';
+        end
+        
+        dt = data.t(2)-data.t(1);
+        sigma = data.position_error;
+        
+        for iStride=1:length(result_stride)
+            stride = result_stride(iStride);
+            
+            % Reduce the total length in some cases
+            if (stride < 10)
+                shortenFactor = stride/10;
+            else
+                shortenFactor = 1;
+            end
+            
+            indices_obs = 1:stride:floor(shortenFactor*length(data.t));
+            indices_all = 1:max(indices_obs);
+            fprintf('dT = %d --- Using %d points with stride %d\n', stride*dt, length(indices_obs), stride);
+            
+            
+            
+            for iS = 1:length(S_range)
+                S = S_range(iS);
+                for T = 1:S
+                    fprintf('\tS=%d, T=%d. Evaluating ensemble',S,T)
+                    K = S+1;
+                    for iEnsemble = 1:totalEnsembles
+                        fprintf('..%d',iEnsemble);
+                        
+                        % contaminate the data for a new ensemble
+                        epsilon_x = data.position_error*randn(length(indices_obs),1);
+                        x_obs = data.x(indices_obs) + epsilon_x;
+                        t_obs = data.t(indices_obs);
+                        
+                        % Fit
+                        spline_x = TensionSpline(t_obs,x_obs,sigma, 'S', S, 'T', T, 'knot_dof', 1);
+                        compute_rms_error_obs = @() sqrt(mean(mean(  (data.x(indices_obs) - spline_x(data.t(indices_obs))).^2,2 ),1));
+                        compute_rms_error_all = @() sqrt(mean(mean(  (data.x(indices_all) - spline_x(data.t(indices_all))).^2,2 ),1));
+                        
+                        % Minimize against just the observed data
+                        lambda_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = TensionSpline.MinimizeMeanSquareError(spline_x,data.t(indices_obs),data.x(indices_obs));
+                        rms_error_obs_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_obs();
+                        rms_error_all_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_all();
+                        dof_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = spline_x.DOFFromVarianceOfTheMean;
+                        [rmse,norm] = TensionSpline.MeanSquareErrorAtAllOrders(spline_x, data.t(indices_obs),data.x(indices_obs));
+                        Q_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = find(rmse./norm < 0.9,1,'last');
+                        
+                        
+                        % Minimize against all the data
+                        lambda_optimal_all(iSlope,iStride,iS,T,iEnsemble) = TensionSpline.MinimizeMeanSquareError(spline_x,data.t(indices_all),data.x(indices_all));
+                        rms_error_obs_optimal_all(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_obs();
+                        rms_error_all_optimal_all(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_all();
+                        dof_optimal_all(iSlope,iStride,iS,T,iEnsemble) = spline_x.DOFFromVarianceOfTheMean;
+                        [rmse,norm] = TensionSpline.MeanSquareErrorAtAllOrders(spline_x, data.t(indices_obs),data.x(indices_obs));
+                        Q_optimal_all(iSlope,iStride,iS,T,iEnsemble) = find(rmse./norm < 0.9,1,'last');
+                        
+                    end
+                    fprintf('\n');
+                    
+                    fprintf('\t\tobs (rms_obs,rms_all,Q)=(%#.3g m, %#.3g m, %d)\n',mean(rms_error_obs_optimal_obs(iSlope,iStride,iS,T,:)),mean(rms_error_all_optimal_obs(iSlope,iStride,iS,T,:)), median(Q_optimal_obs(iSlope,iStride,iS,T,:)));
+                    
+                    fprintf('\t\tall (rms_obs,rms_all,Q)=(%#.3g m, %#.3g m, %d)\n',mean(rms_error_obs_optimal_all(iSlope,iStride,iS,T,:)),mean(rms_error_all_optimal_all(iSlope,iStride,iS,T,:)), median(Q_optimal_all(iSlope,iStride,iS,T,:)));
+                    
+                end
+            end
+            
+        end
+    end
+    
+    save(filename, 'S_range', 'totalEnsembles', 'slopes', 'result_stride','lambda_optimal_all','rms_error_obs_optimal_all','rms_error_all_optimal_all','dof_optimal_all','Q_optimal_all','lambda_optimal_obs','rms_error_obs_optimal_obs','rms_error_all_optimal_obs','dof_optimal_obs','Q_optimal_obs');
+
+end
+
+rms_error_obs_optimal_obs(rms_error_obs_optimal_obs==0.0) = nan;
+
+% minimum error across S,T, for each ensemble
+min_rms_error = min(min(rms_error_obs_optimal_obs,[],3),[],4);
+
+% now we normalize them (min error should be 1.0 for each ensemble)
+rel_rms_error = rms_error_obs_optimal_obs./min_rms_error;
+
+% now compute the ensemble average increase in error
+rel_rms_error = mean(rel_rms_error,5);
+
+% now flatten across stride and slope
+rel_rms_error_SvsT =squeeze(mean(mean(rel_rms_error)));
+
+% fprintf('\n\n');
+% fprintf('\\begin{tabular}{r | llll} stride & full dof & reduced dof & blind initial & blind optimal \\\\ \\hline \\hline \n');
+% for iSlope = 1:length(slopes)
+%     
+%     fprintf('%d slope &&&&  \\\\ \\hline \n',slopes(iSlope));
+%     for iStride=1:length(result_stride)
+% %         fprintf('%d & %#.3g m^2 (%#.3g) &  %#.3g m^2 (%#.3g) &  %#.3g m^2 (%#.3g) &  %#.3g m^2 (%#.3g) \\\\ \n', result_stride(iStride), mse_full_dof_true_optimal_mean(iStride,iSlope), dof_se_full_dof_true_optimal(iStride,iSlope), dmse_reduced_dof_true_optimal_mean(iStride,iSlope), dof_se_reduced_dof_true_optimal(iStride,iSlope), dmse_reduced_dof_blind_initial_mean(iStride,iSlope), dof_se_reduced_dof_blind_initial(iStride,iSlope), dmse_reduced_dof_blind_optimal_mean(iStride,iSlope), dof_se_reduced_dof_blind_optimal(iStride,iSlope) )  ;
+%         fprintf('%d & %#.3g m$^2$ (%#.3g) &  %+.1f\\%% (%#.3g) &  %+.1f\\%% (%#.3g) &  %+.1f\\%% (%#.3g) \\\\ \n', result_stride(iStride), mse_full_dof_true_optimal_mean(iStride,iSlope), dof_se_full_dof_true_optimal(iStride,iSlope), 100*dmse_reduced_dof_true_optimal_mean(iStride,iSlope)./mse_full_dof_true_optimal_mean(iStride,iSlope), dof_se_reduced_dof_true_optimal(iStride,iSlope), 100*dmse_reduced_dof_blind_initial_mean(iStride,iSlope)./mse_full_dof_true_optimal_mean(iStride,iSlope), dof_se_reduced_dof_blind_initial(iStride,iSlope), 100*dmse_reduced_dof_blind_optimal_mean(iStride,iSlope)./mse_full_dof_true_optimal_mean(iStride,iSlope), dof_se_reduced_dof_blind_optimal(iStride,iSlope) )  ;
+%     end
+%     
+% end
+% fprintf('\\end{tabular} \n');
+
+rms_error_SequalT = zeros(totalSlopes,length(result_stride),length(S_range),totalEnsembles);
+for iS = 1:length(S_range)
+    rms_error_SequalT(:,:,iS,:) = rms_error_obs_optimal_obs(:,:,iS,iS,:);
+end
+
+% minimum error across S,T, for each ensemble
+min_rms_error = min(rms_error_SequalT,[],3);
+
+% now we normalize them (min error should be 1.0 for each ensemble)
+rel_rms_error = rms_error_SequalT./min_rms_error;
+
+% now compute the ensemble average increase in error
+rel_rms_error = mean(rel_rms_error,4);
+
+rel_rms_error_SlopeVsS = 100*(squeeze(mean(rel_rms_error,2)) - 1);
+
+fprintf('\n\n');
+fprintf('\\begin{tabular}{r | llll}  & S=1 & S=2 & S=3 & S=4 \\\\ \\hline \\hline \n');
+for iSlope = 1:length(slopes)
+    
+    fprintf('$\\omega^{%d}$ & %+.1f\\%% & %+.1f\\%% & %+.1f\\%% & %+.1f\\%%  \\\\ \n',slopes(iSlope),rel_rms_error_SlopeVsS(iSlope,1),rel_rms_error_SlopeVsS(iSlope,2),rel_rms_error_SlopeVsS(iSlope,3),rel_rms_error_SlopeVsS(iSlope,4));    
+end
+fprintf('\\end{tabular} \n');
