e56b453c7863db50d79534e10b006e3f363400c0
JeffreyEarly <jeffrey@jeffreyearly.com>
2019-03-21 14:49:41 -0700

    Update ShowGPSFitWithMeanRemoved.m

--- Matlab/ShowGPSFitWithMeanRemoved.m
+++ Matlab/ShowGPSFitWithMeanRemoved.m
@@ -11,27 +11,25 @@ choiceDrifter = 6;
 drifters = open('sample_data/raw_rho1_drifters.mat');
 
 t_drifter = (drifters.date{choiceDrifter}-drifters.lastDeployment)*24*60*60;
-spline = GPSTensionSpline(t_drifter,drifters.lat{choiceDrifter},drifters.lon{choiceDrifter},'shouldUseRobustFit',1);
+lat = drifters.lat{choiceDrifter};
+lon = drifters.lon{choiceDrifter};
+lon0 = min(lon)+(max(lon)-min(lon))/2;
+spline = GPSTensionSpline(t_drifter,lat,lon,'shouldUseRobustFit',1);
 
-t = t_drifter;
-x = spline.x;
-y = spline.y;
-
-spline_xy = BivariateTensionSpline(t,x,y,StudentTDistribution(8.5,4.5),'shouldUseRobustFit',1);
-spline_xy.estimateOutlierDistribution();
-mse1 = spline_xy.minimizeExpectedMeanSquareErrorInNoiseRange();
+spline.estimateOutlierDistribution();
+mse1 = spline.minimizeExpectedMeanSquareErrorInNoiseRange();
 
 tq = linspace(min(spline.t),max(spline.t),10*length(spline.t));
-[xq,yq] = spline_xy.xyAtTime(tq);
+[xq,yq] = spline.xyAtTime(tq);
 
 figure
-scatter(x,y,(2.5)^2,'filled', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k'), hold on
+scatter(spline.x,spline.y,(2.5)^2,'filled', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k'), hold on
 plot(xq,yq,'k','LineWidth',1.5),axis equal
 
-spline_xy.setSigmaFromFullTensionSolution();
-mse2 = spline_xy.minimizeExpectedMeanSquareErrorInNoiseRange();
+spline.setSigmaFromFullTensionSolution();
+mse2 = spline.minimizeExpectedMeanSquareErrorInNoiseRange();
 
-[xq,yq] = spline_xy.xyAtTime(tq);
+[xq,yq] = spline.xyAtTime(tq);
 plot(xq,yq)
 
 t_drifter = (drifters.date{7}-drifters.lastDeployment)*24*60*60;
