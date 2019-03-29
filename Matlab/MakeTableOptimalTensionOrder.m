6c60dc3e3f09495e8ffeac6293f05febe0bb02dc
JeffreyEarly <jeffrey@jeffreyearly.com>
2019-03-29 15:10:02 -0700

    Redoing the tables with new refactored code.

--- Matlab/MakeTableOptimalTensionOrder.m
+++ Matlab/MakeTableOptimalTensionOrder.m
@@ -4,124 +4,86 @@ shouldLoadExistingTable = 1;
 
 filename = 'OptimalTensionOrderTable.mat';
 
-if shouldLoadExistingTable == 1
-    load(filename);
-    
-    totalSlopes = length(slopes);
+if exist(filename,'file')
+    load(filename);    
 else
     slopes = [-2; -3; -4];
+    slopes = -3;
     S_range = 1:5;
-    result_stride = 2.^(2:7)';
-    totalEnsembles = 5;
+    strides = [5;20;80;200];
     
     totalSlopes = length(slopes);
-
-    lambda_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    rms_error_obs_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    rms_error_all_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    dof_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    Q_optimal_all = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    totalStrides = length(strides);
+    totalEnsembles = 5;
     
-    lambda_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    rms_error_obs_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    rms_error_all_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    dof_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
-    Q_optimal_obs = zeros(totalSlopes,length(result_stride),length(S_range),length(S_range),totalEnsembles);
+    % matern signal parameters
+    sigma_u = 0.20;
+    base_dt = 5; % for whatever reason, we chose this as the primary dt
+    t_damp = 30*60;
+    n = 250;
+    
+    optimal_lambda = nan(totalSlopes,length(strides),length(S_range),length(S_range),totalEnsembles);
+    optimal_rmse = nan(totalSlopes,length(strides),length(S_range),length(S_range),totalEnsembles);
+    optimal_neff = nan(totalSlopes,length(strides),length(S_range),length(S_range),totalEnsembles);
         
     for iSlope = 1:length(slopes)
-        
         slope = slopes(iSlope);
-        fprintf('slope %d, ',slope);
+        fprintf('slope %d, ',slope);        
         
-        if slope == -2
-            data = load('sample_data/SyntheticTrajectories.mat');
-            outputFile = 'OptimalParameters.mat';
-        elseif slope == -3
-            data = load('sample_data/SyntheticTrajectoriesSlope3.mat');
-            outputFile = 'OptimalParametersSlope3.mat';
-        elseif slope == -4
-            data = load('sample_data/SyntheticTrajectoriesSlope4.mat');
-            outputFile = 'OptimalParametersSlope4.mat';
-        end
-        
-        dt = data.t(2)-data.t(1);
-        sigma = data.position_error;
-        
-        for iStride=1:length(result_stride)
-            stride = result_stride(iStride);
+        for iStride=1:length(strides)
+            stride = strides(iStride);
+            dt = stride*base_dt;
+            fprintf('stride %d, ',stride);
             
-            % Reduce the total length in some cases
-            if (stride < 10)
-                shortenFactor = stride/10;
-            else
-                shortenFactor = 1;
-            end
-            
-            indices_obs = 1:stride:floor(shortenFactor*length(data.t));
-            indices_all = 1:max(indices_obs);
-            fprintf('dT = %d --- Using %d points with stride %d\n', stride*dt, length(indices_obs), stride);
-            
-            
-            
-            for iS = 1:length(S_range)
-                S = S_range(iS);
-                for T = 1:S
-                    fprintf('\tS=%d, T=%d. Evaluating ensemble',S,T)
-                    K = S+1;
-                    for iEnsemble = 1:totalEnsembles
-                        fprintf('..%d',iEnsemble);
+            for iEnsemble = 1:totalEnsembles
+                fprintf('..%d',iEnsemble);
+                
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % Generate the signal
+                cv=maternoise(dt,n,sigma_u*sqrt(2),abs(slope),1/t_damp);
+                cx = cumtrapz(cv)*dt;
+                data = struct('t',dt*(0:n-1)','x',real(cx));
+                
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % Generate the noise
+                sigma = 10;
+                noiseDistribution = NormalDistribution(sigma);
+                epsilon = noiseDistribution.rand(size(data.x));
+                
+                x_obs = data.x + epsilon;
+                t_obs = data.t;
+                
+                compute_rms_error = @(spline) sqrt(mean(mean(  (data.x - spline(data.t)).^2,2 ),1));
+                
+                % For a given ensemble, we now try fitting to various
+                % spline orders with varying tension order
+                for iS = 1:length(S_range)
+                    S = S_range(iS);
+                    for T = 1:S
+                        K = S+1;
                         
-                        % contaminate the data for a new ensemble
-                        epsilon_x = data.position_error*randn(length(indices_obs),1);
-                        x_obs = data.x(indices_obs) + epsilon_x;
-                        t_obs = data.t(indices_obs);
+                        spline = TensionSpline(t_obs,x_obs,noiseDistribution, 'S', S, 'T', T, 'knot_dof', 1,'lambda',Lambda.fullTensionExpected);
+                        spline.minimizeMeanSquareError(data.t,data.x);
                         
-                        % Fit
-                        spline_x = TensionSpline(t_obs,x_obs,sigma, 'S', S, 'T', T, 'knot_dof', 1);
-                        compute_rms_error_obs = @() sqrt(mean(mean(  (data.x(indices_obs) - spline_x(data.t(indices_obs))).^2,2 ),1));
-                        compute_rms_error_all = @() sqrt(mean(mean(  (data.x(indices_all) - spline_x(data.t(indices_all))).^2,2 ),1));
-                        
-                        % Minimize against just the observed data
-                        lambda_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = TensionSpline.MinimizeMeanSquareError(spline_x,data.t(indices_obs),data.x(indices_obs));
-                        rms_error_obs_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_obs();
-                        rms_error_all_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_all();
-                        dof_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = spline_x.DOFFromVarianceOfTheMean;
-                        [rmse,norm] = TensionSpline.MeanSquareErrorAtAllOrders(spline_x, data.t(indices_obs),data.x(indices_obs));
-                        Q_optimal_obs(iSlope,iStride,iS,T,iEnsemble) = find(rmse./norm < 0.9,1,'last');
-                        
-                        
-                        % Minimize against all the data
-                        lambda_optimal_all(iSlope,iStride,iS,T,iEnsemble) = TensionSpline.MinimizeMeanSquareError(spline_x,data.t(indices_all),data.x(indices_all));
-                        rms_error_obs_optimal_all(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_obs();
-                        rms_error_all_optimal_all(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error_all();
-                        dof_optimal_all(iSlope,iStride,iS,T,iEnsemble) = spline_x.DOFFromVarianceOfTheMean;
-                        [rmse,norm] = TensionSpline.MeanSquareErrorAtAllOrders(spline_x, data.t(indices_obs),data.x(indices_obs));
-                        Q_optimal_all(iSlope,iStride,iS,T,iEnsemble) = find(rmse./norm < 0.9,1,'last');
-                        
-                    end
-                    fprintf('\n');
-                    
-                    fprintf('\t\tobs (rms_obs,rms_all,Q)=(%#.3g m, %#.3g m, %d)\n',mean(rms_error_obs_optimal_obs(iSlope,iStride,iS,T,:)),mean(rms_error_all_optimal_obs(iSlope,iStride,iS,T,:)), median(Q_optimal_obs(iSlope,iStride,iS,T,:)));
-                    
-                    fprintf('\t\tall (rms_obs,rms_all,Q)=(%#.3g m, %#.3g m, %d)\n',mean(rms_error_obs_optimal_all(iSlope,iStride,iS,T,:)),mean(rms_error_all_optimal_all(iSlope,iStride,iS,T,:)), median(Q_optimal_all(iSlope,iStride,iS,T,:)));
-                    
+                        optimal_lambda(iSlope,iStride,iS,T,iEnsemble) = spline.lambda;
+                        optimal_rmse(iSlope,iStride,iS,T,iEnsemble) = compute_rms_error(spline);
+                        optimal_neff(iSlope,iStride,iS,T,iEnsemble) = spline.effectiveSampleSizeFromVarianceOfTheMean;
+                    end                   
                 end
             end
-            
+            fprintf('\n');
         end
     end
     
-    save(filename, 'S_range', 'totalEnsembles', 'slopes', 'result_stride','lambda_optimal_all','rms_error_obs_optimal_all','rms_error_all_optimal_all','dof_optimal_all','Q_optimal_all','lambda_optimal_obs','rms_error_obs_optimal_obs','rms_error_all_optimal_obs','dof_optimal_obs','Q_optimal_obs');
+    save(filename, 'S_range', 'totalEnsembles', 'slopes', 'strides','optimal_lambda','optimal_rmse','optimal_neff');
 
 end
 
-rms_error_obs_optimal_obs(rms_error_obs_optimal_obs==0.0) = nan;
-
 % minimum error across S,T, for each ensemble
-min_rms_error = min(min(rms_error_obs_optimal_obs,[],3),[],4);
+min_rms_error = min(min(optimal_rmse,[],3),[],4);
 
 % now we normalize them (min error should be 1.0 for each ensemble)
-rel_rms_error = rms_error_obs_optimal_obs./min_rms_error;
+rel_rms_error = optimal_rmse./min_rms_error;
 
 pct_range = 0.6827; % Chosen to match 1-sigma for a Gaussian (these are not Gaussian).
 rel_rms_error_SvsT = zeros(length(S_range),length(S_range));
@@ -179,9 +141,9 @@ fprintf(' \\bottomrule \n\\end{tabular} \n');
 % end
 % fprintf('\\end{tabular} \n');
 
-rms_error_SequalT = zeros(totalSlopes,length(result_stride),length(S_range),totalEnsembles);
+rms_error_SequalT = zeros(totalSlopes,length(strides),length(S_range),totalEnsembles);
 for iS = 1:length(S_range)
-    rms_error_SequalT(:,:,iS,:) = rms_error_obs_optimal_obs(:,:,iS,iS,:);
+    rms_error_SequalT(:,:,iS,:) = optimal_rmse(:,:,iS,iS,:);
 end
 
 % minimum error across S,T, for each ensemble
